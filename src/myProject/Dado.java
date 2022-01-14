6df36f361a2df6e16eb2e768196c400c52c7628b
Javier-C0L <javier.castrillon@correounivalle.edu.co>
2022-01-14 03:19:04 -0500

    cambio Dado

--- src/myProject/Dado.java
+++ src/myProject/Dado.java
@@ -1,15 +1,46 @@
+/*
+ * Programación Interactiva
+ * Autor:
+ * Javier Castrillon 1827486-2711
+ * Alejandro Villamil 2042419-
+ * Caso 1: Mini proyecto
+ */
 package myProject;
 
 import java.util.Random;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 /**
  * Class Dado generate a Random value between 1 and 6
  * @author Alejandro Villamil
+ * @author Javier Castrillon
  * @version v.1.0.0 date 09/12/2021
  */
 
-public class Dado {
+public class Dado extends JButton{
+    /**
+     *	The attributes
+     */
+    private static int dadoSize = 80;
+    private static int maxDados = 10;
     private int cara;
+    private ImageIcon image;
+
+
+
+    /**
+     * Method that generate
+     *
+     */
+    public Dado(int caraDado) {
+        this.cara = caraDado;
+
+        image = new ImageIcon("src/resources/"+caraDado+".png");
+    }
 
     /**
      * Method that generate an random value to cara
@@ -18,7 +49,23 @@ public class Dado {
     public int getCara() {
         Random aleatorio = new Random();
         cara = aleatorio.nextInt(6) + 1;
-
+        image = new ImageIcon("src/resources/"+cara+".png");
         return cara;
+    }
+
+    /**
+     * Method that generate an image
+     * @return one of six images from the Dado
+     */
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    /**
+     * Method that generate an image
+     * @param image
+     */
+    public void setImage(ImageIcon image) {
+        this.image = image;
     }
 }
--- src/myProject/GeekOutMasters.java
+++ src/myProject/GeekOutMasters.java
@@ -1,5 +1,12 @@
 package myProject;
 
 public class GeekOutMasters {
+    public String[] rutaF = {"src/resources/1.png", "src/resources/2.png", "src/resources/3.png",
+            "src/resources/4.png", "src/resources/5.png", "src/resources/6.png"};
+    private int dadoSize = 80, maxDados = 10;
+    private Dado[] tablero;
 
+    public GeekOutMasters(){
+
+    }
 }
